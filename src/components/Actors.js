9a433626718fa4dc053711904a6a917dcf5e1341
Andrea T <tostonar@gmail.com>
2019-11-04 14:50:48 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,17 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      { actors.map((actor, i) =>
+        <div className="actor" key={i}>
+          <h2>Name: {actor.name}</h2>
+          <p>Movies: </p>
+          <ul>
+            {actor.movies.map((movie, i) => <li key={i}>{movie}</li>)}
+          </ul>
+
+        </div>
+        )}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const movieElements = movies.map((movie, index) => {
+    return (
+      <div key={index}>
+        <h2>Title: {movie.title}</h2>
+        <h4>Time: {movie.time}</h4>
+        <p>Genres:</p>
+        <ul>
+          {movie.genres.map((genre,index) => <li key={index}>{genre}</li>)}
+        </ul>
+      </div>
+    )
+  })
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        { movieElements }
     </div>
   );
 };
